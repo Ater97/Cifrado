b182ee8093b68d49fabd8a049e1f10f6b44b9f0e
Sebastián Orantes Chang <ater7991@gmail.com>
2017-10-04 21:37:47 -0600

    first blush
    -obtener el n primo
    -phi
    -coprimo

--- Encryption/Encryption/Program.cs
+++ Encryption/Encryption/Program.cs
@@ -10,6 +10,14 @@ namespace Encryption
     {
         static void Main(string[] args)
         {
+            Utilities U = new Utilities();
+
+
+            //Console.WriteLine(U.GetPrimeNumber(Convert.ToInt32(Console.ReadLine())));
+            int a =int.Parse(Console.ReadLine());
+            Console.WriteLine(U.Coprime(a, int.Parse(Console.ReadLine())));
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Encryption/Encryption/Utilities.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Utilities
+    {
+        public int GetPrimeNumber(int num)
+        {
+            int i, n = 4, cont = 2;
+            if (num > 2) // 2 & 3, too low.
+            {
+                while (cont < num)
+                {
+                    i = 2;
+                    while (i <= n)
+                    {
+                        if (i == n)
+                        {
+                            cont = cont + 1;
+                        }
+                        else
+                        {
+                            if (n % i == 0)
+                            {
+                                i = n;
+                            }
+                        }
+                        i = i + 1;
+                    }
+                    n = n + 1;
+                }
+                return n - 1;
+            }
+            return 0;
+        }
+
+        public int[] Phi(int p, int q)
+        {
+            int[]NyZ = new int[2];
+            NyZ[0] = p * q;
+            NyZ[1] = (p - 1) * (q - 1);
+            return NyZ;
+        }
+
+        private bool CheckCoprime(int A,int B)
+        {
+            while (A != 0 && B != 0)
+            {
+                if (A > B)
+                    A %= B;
+                else
+                    B %= A;
+            }
+
+            return Math.Max(A, B) == 1 ? true:false;
+
+        }
+
+        public int getFirstCoprime(int A, int inicial, int final)
+        {
+            for (int i = inicial; i < final ; i++)
+            {
+                if (CheckCoprime(A, i))
+                    return i;
+            }
+            return 0;
+        }
+
+
+    }
+}
+    
