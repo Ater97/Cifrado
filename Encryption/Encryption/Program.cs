f8a29123d9c6a597031a89fd16809dc31dc7c779
Sebastián Orantes Chang <ater7991@gmail.com>
2017-10-04 21:55:03 -0600

    Public key

--- Encryption/Encryption/Program.cs
+++ Encryption/Encryption/Program.cs
@@ -10,13 +10,12 @@ namespace Encryption
     {
         static void Main(string[] args)
         {
-            Utilities U = new Utilities();
+            RCA rca = new RCA();
+            int np = int.Parse(Console.ReadLine());
+            Console.WriteLine(rca.PublicKey(np, int.Parse(Console.ReadLine())));
 
 
-            //Console.WriteLine(U.GetPrimeNumber(Convert.ToInt32(Console.ReadLine())));
-            int a =int.Parse(Console.ReadLine());
-            Console.WriteLine(U.Coprime(a, int.Parse(Console.ReadLine())));
-
+         
             Console.ReadKey();
         }
     }
---
+++ Encryption/Encryption/RCA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class RCA
+    {
+        Utilities Ut = new Utilities();
+
+        public string PublicKey(int np, int nq) // n prime number
+        {
+            int p = Ut.GetPrimeNumber(np);
+            int q = Ut.GetPrimeNumber(nq);
+            int[] NyZ = Ut.Phi(p, q);
+            int k = Ut.getFirstCoprime(NyZ[1], 20, 100);
+            return NyZ[0].ToString() + "," + k.ToString();
+        }
+        //k* j = 1(mod z) Private key
+    }
+}
