49f8ed51f35915b3ee67b4e6080cac90bf62489b
Unknown <jcmansillam@correo.url.edu.gt>
2017-10-05 07:34:02 -0600

    Inicio de la linea de comandos

---
+++ Encryption/Encryption/Utilities/CommandLine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption.Utilities
+{
+    static class CommandLine
+    {
+        public static bool isCorrectCommand(string command)
+        {
+            string[] commands = command.Split(' ');
+            string filePath = string.Empty;
+            if (command == "") return false;
+            if (command.Length > 3) return false;
+            return isEncrypting(commands);
+            
+        }
+
+        private static bool fileExist(string command, string filePath)
+        {
+            FileStream original = new FileStream(filePath, FileMode.Open);
+            if (command == "-f" || command == "-F")
+            {
+                return true;
+            }
+            Console.WriteLine("ERROR -f command is missing!");
+            Console.ReadKey();
+            return false;
+        }
+
+        private static bool isEncrypting(string [] commands)
+        {
+            if (commands[0] == "-c" || commands[0] == "-C")
+            {
+                return fileExist(commands[1], commands[2]);
+            }
+            if (commands[0] == "-d" || commands[0] == "-D")
+            {
+                return fileExist(commands[1], commands[2]);
+            }
+            Console.WriteLine("ERROR -c or -d command is missing!");
+            Console.ReadKey();
+            return false;
+        }
+    }
+}
